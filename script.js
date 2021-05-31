38a90c53d766c830394263dbd3edcb2e74be0c14
U_U <49274147+viettran0801@users.noreply.github.com>
2021-05-31 19:07:10 +0700

    append to top

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ window.addEventListener('blur', () => {
     const text = document.createTextNode(`${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}:  >>>>> thoat ra`);
     tag.style.color = 'red';
     tag.appendChild(text)
-    log.appendChild(tag);
+    log.prepend(tag);
 });
 
 window.addEventListener('focus', () => {
@@ -15,5 +15,5 @@ window.addEventListener('focus', () => {
     const text = document.createTextNode(`${today.getHours()}:${today.getMinutes()}:${today.getSeconds()}:  <<<<< quay lai`);
     tag.style.color = 'green';
     tag.appendChild(text)
-    log.appendChild(tag);
+    log.prepend(tag);
 })
